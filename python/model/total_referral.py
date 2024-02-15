a16af516fbb4c47a78303dd8d4195a4304f8bfe0
ahmadbilal2023 <ahmadbilal20152016@gmail.com>
2024-02-15 09:25:54 +0500

    update some models

---
+++ python/model/total_referral.py
@@ -0,0 +1,10 @@
+from sqlmodel import Field, SQLModel
+
+# pin models start here -------------- ⬇️
+
+
+class total_referral(SQLModel, table=True):
+    id: int | None = Field(default=None, primary_key=True)
+    pin: str
+    created_at: str
+    updated_at: str
